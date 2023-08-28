498c034e8782ea46eb3cbc6e5b3e614ed8b51c0e
ColdDionysus <gautampukar02@gmail.com>
2023-08-28 19:15:31 +0545

    Subtask

--- modules/subtask/subtask.api.js
+++ modules/subtask/subtask.api.js
@@ -1,3 +1,52 @@
 const router = require("express").Router();
+const subtaskController = require("./subtask.controller");
+
+router.post("/", (req, res, next) => {
+  try {
+    const { payload } = req.body;
+    const result = subtaskController.create(payload);
+    res.json({ data: result, msg: "Success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/", (req, res, next) => {
+  try {
+    const result = subtaskController.list();
+    res.json({ data: result, msg: "Success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = subtaskController.getById(id);
+    res.json({ data: result, msg: "Sucess" });
+  } catch (e) {
+    next(e);
+  }
+});
+router.put("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { payload } = req.body;
+    const result = subtaskController.updateById(id, payload);
+    res.json({ data: result, msg: "Success" });
+  } catch (e) {
+    next(e);
+  }
+});
+router.delete("/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = subtaskController.deleteById(id);
+    res.json({ data: result, msg: "Success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
