cc6e63224ef714594870ab7e948251807aaa69d4
ColdDionysus <gautampukar02@gmail.com>
2023-08-28 18:05:26 +0545

    CRUD

--- modules/todo/todo.api.js
+++ modules/todo/todo.api.js
@@ -1,3 +1,32 @@
 const router = require("express").Router();
+const todoController = require("./todo.controller");
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await todoController.list();
+    res.json({ data: result, msg: "Success" });
+  } catch (e) {
+    next(e);
+  }
+});
+router.get("/:id", (req, res, next) => {
+  try {
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { payload } = req.body;
+    const result = await todoController.create(payload);
+    res.json({ data: result, msg: "Success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", (req, res, next) => {});
+
+router.delete("/:id", (req, res, next) => {});
 
 module.exports = router;
---
+++ modules/todo/todo.controller.js
@@ -0,0 +1,7 @@
+const create = () => {};
+const list = () => {};
+const getById = () => {};
+const updateById = () => {};
+const deleteById = () => {};
+
+module.exports = { create, list, getById, updateById, deleteById };
